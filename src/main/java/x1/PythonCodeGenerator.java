e089c94e5d949f98d590c84aa4686c0f8d47916d
Alex Collins <alex_collins@intuit.com>
2023-12-27 21:56:04 -0800

    pythos

---
+++ src/main/java/x1/PythonCodeGenerator.java
@@ -0,0 +1,149 @@
+package x1;
+
+import x1.model.*;
+
+public class PythonCodeGenerator extends CLikeCodeGenerator {
+
+  @Override
+  public String getLanguage() {
+    return "python";
+  }
+
+  @Override
+  public String getExtension() {
+    return "py";
+  }
+
+  @Override
+  public void visit(VariableDeclarationNode node) {
+    node.getIdentifier().accept(this);
+    append(" = ");
+    node.getExpression().accept(this);
+  }
+
+  @Override
+  public void visit(AssignmentStatementNode node) {
+    node.getIdentifier().accept(this);
+    append(" = ");
+    node.getExpression().accept(this);
+  }
+
+  @Override
+  public void visit(IfStatementNode node) {
+    append("if ");
+    node.getExpression().accept(this);
+    append(":\n");
+    indent++;
+    node.getBlock().accept(this);
+    indent--;
+    if (node.getElseBlock() != null) {
+      indent();
+      append("else:\n");
+      indent++;
+      node.getElseBlock().accept(this);
+      indent--;
+    }
+  }
+
+  @Override
+  public void visit(TypeDeclarationNode node) {
+    append("class ");
+    node.getIdentifier().accept(this);
+    append(":\n");
+    indent++;
+    node.getFieldDeclarations()
+        .forEach(
+            fieldDeclaration -> {
+              indent();
+              fieldDeclaration.accept(this);
+              append("\n");
+            });
+    indent--;
+  }
+
+  @Override
+  public void visit(FieldDeclarationNode node) {}
+
+  @Override
+  public void visit(MethodDeclarationNode node) {
+    append("def ");
+    node.getIdentifier().accept(this);
+    append("(");
+    node.getParameterList().accept(this);
+    append("):\n");
+    indent++;
+    node.getBlock().accept(this);
+    indent--;
+  }
+
+  @Override
+  public void visit(BlockNode node) {
+    node.getStatements()
+        .forEach(
+            statement -> {
+              indent();
+              statement.accept(this);
+              append("\n");
+            });
+  }
+
+  @Override
+  public void visit(ParameterNode node) {
+    node.getIdentifier().accept(this);
+  }
+
+  @Override
+  public void visit(TypeNode node) {
+    node.getIdentifier().accept(this);
+    if (node.isArray()) {
+      append("[]");
+    }
+  }
+
+  @Override
+  public void visit(ForDeclarationNode node) {
+    // convert to while loop
+    node.getVariableDeclaration().accept(this);
+    append("\n");
+    indent();
+    append("while ");
+    node.getExpression().accept(this);
+    append(":\n");
+    indent++;
+    node.getBlock().accept(this);
+    indent();
+    node.getAssignmentStatement().accept(this);
+    indent--;
+  }
+
+  @Override
+  public void visit(ForEachDeclarationNode node) {
+    append("for ");
+    node.getIdentifier().accept(this);
+    append(" in ");
+    node.getExpression().accept(this);
+    append(":\n");
+    indent++;
+    node.getBlock().accept(this);
+    indent--;
+  }
+
+  @Override
+  public void visit(ReturnStatementNode node) {
+    append("return ");
+    node.getExpression().accept(this);
+    append("\n");
+  }
+
+  @Override
+  public void visit(ArrayExpressionNode node) {
+    append("[");
+    for (int i = 0; i < node.getExpressions().size(); i++) {
+      if (i > 0) {
+        append(", ");
+      }
+      node.getExpressions().get(i).accept(this);
+    }
+    append("]");
+  }
+}
---
+++ src/test/resources/example.py
@@ -0,0 +1,12 @@
+def sum(v, n):
+  i = 0
+  while i < n:
+    if i % 2 == 0:
+      v = v + i
+
+    i = i + 1
+  y = [1, 2]
+  for x in y:
+
+  return v
+
